d3e2c1bc8fab62f6b8db68f34e0f47bc5f089ec0
Mathew-B-SDGS <mathew.baines@nhs.net>
2023-06-16 14:37:57 +0100

    finnished problem 1

--- Q1.py
+++ Q1.py
@@ -17,19 +17,33 @@ Given a list of genes, e.g. 'MLH1 AHR BRCA2 BRCA1 MLH1 CAT'
 with open("genes.txt", "r") as file:
     #reads the file and splits it, removing any spaces
     genes_list = file.read().split()
-    #changes the list into a set to easily remove the duplicates as sets dont allow dups.
-    genes_set = set(genes_list)
 
-# print(genes_set)
-print("there are",len(genes_set),"unique genes in this set. which are listed as",(genes_set),)
 
-duplicates=[]
-singles=[]
-for genes in genes_list:
-    if genes in singles:
-        duplicates.append(genes)
-    else:
-        singles.append(genes)
-print(singles,duplicates)
+#function to solve prob A.
+def remove_duplicates(gene_list):
+#changes the list into a set to easily remove the duplicates as sets dont allow dups.
+    genes_set = set(gene_list)
+    return genes_set
+
+#function to solve prob B.
+def list_duplicates(gene_list):
+    duplicates=[]
+    singles=[]
+    #a loop that iterates over the list adding duplicate values to a new variable
+    for genes in gene_list:
+        if genes in singles:
+            duplicates.append(genes)
+        else:
+            singles.append(genes) 
+    return duplicates
+
+a=remove_duplicates(genes_list)
+b= list_duplicates(genes_list)
+
+print("for problem A, the values are",", ".join(a), "\n for Problem B the answers are",", ".join(b))
+
+
+
+
 
 
