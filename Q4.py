f8b76f0b6807dc09f5c6e3ffe7ddd9e39c802e96
Mathew-B-SDGS <mathew.baines@nhs.net>
2023-06-21 12:37:25 +0100

    commented a bit more

--- Q4.py
+++ Q4.py
@@ -5,39 +5,23 @@ Write a function that takes as input integers N and L to generate N million rand
 
 '''
 import random
-#N number of short reads, 
-#L length of the short reads.
-def generator(length,number):
-    #Dict assigning a value key for each Base
-    base_coding="ATCG"
-    read=""
-    #Multiplying the N input value to a factor of a million 
-    n_million=int(number)*100000
-    #Opening a Fastq file to keep data
-    with open("seq.fastq","w") as file:
-        #loop through the set range
-        for number in range(n_million):
-            #formatted as a Fastq File, with the @ number and read name 
-            read = "\n>ReadNumber" + str(number) + "\n"
-            #loop to determine the length of sequence. 
-            for length_read in range(length):
-                #randomly choose a number
-                base_random=(random.randint(0,3))
-                #use the random number to select a base from the dict
-                read+=base_coding[base_random]
-            #write out each variable 
-            file.write(read)
-# had issues with run time taking ages, as the read variable was getting to long, changed code to write the variable to the File after each loop.
 
-print(generator(50,5))
+# N: number of short reads
+# L: length of the short reads
+def generator(length, number):
+    base_coding = "ATCG"  #  DNA bases
+    read = ""  # Variable to store  read
+    n_million = int(number) * 100000  # Total number of reads to generate times a million 
+    
+    with open("seq.fastq", "w") as file:  # Open a file for writing, choose to format this into a FASTA. 
+        for number in range(n_million):  # Generate reads based on the total number
+            read = "\n>ReadNumber" + str(number) + "\n"  # Identifier for the read in fasta format
+            for length_read in range(length):  # Generate the sequence of the read
+                base_random = random.randint(0, 3)  # Generate a random index for the base using the random module. 
+                read += base_coding[base_random]  # Add the randomly selected base to the read
+            file.write(read)  # Write the read to the file
+
+# Example usage
+generator(100, 10)  # Generate 10*1,000,000 short reads of length 100bp
 
 
-
-
-'''
-make a range of N 
-loop through the range 
-make a header based on N 
-for range of L 
-random base 
-'''
