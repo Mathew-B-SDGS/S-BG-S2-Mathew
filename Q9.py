e1934e118527a40efe1b036ce34041cf82a097bb
Mathew-B-SDGS <mathew.baines@nhs.net>
2023-06-22 17:57:56 +0100

    added files

---
+++ Q9.py
@@ -0,0 +1,25 @@
+### Question 9 (Rosalind question) ###
+''' 
+An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
+
+Given a DNA string t corresponding to a coding strand, its transcribed RNA string u is formed by replacing all occurrences of 'T' in t with 'U' in u.
+
+Given: A DNA string t having length at most 1000 nt.
+
+Return: The transcribed RNA string of t
+
+
+'''
+test = 'GATGGAACTTGACTACGTAAATT'
+
+
+def RNA(test):
+    rnatest=""
+    for base in test:
+        if base=="T":
+            rnatest+="U"
+        else:
+            rnatest+=base
+    return rnatest
+
+print(RNA(test))
