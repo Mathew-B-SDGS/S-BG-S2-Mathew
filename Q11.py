deb966ca44ad5d49ada72af32091e1f0ee46dec4
Mathew-B-SDGS <mathew.baines@nhs.net>
2023-06-22 17:57:21 +0100

    worked on this one with sam

---
+++ Q11.py
@@ -0,0 +1,48 @@
+### Question 11 ### 
+'''
+Write a function to generate checksums (MD5sum) of files contained within a given direcroy. You must use the os module to generate the checksums.  
+The function must accept a path to an input directory (as a string), calculate the checksums and save the checksums generated to a new file.
+
+'''
+
+
+import os   
+import subprocess
+
+from pathlib import Path
+
+def check(input_directory):
+    p = Path(input_directory)
+    files = list(p.glob("*")) 
+    md5file=""
+    for file in files:
+        task = subprocess.Popen(['md5sum',file],
+                                stdin = subprocess.PIPE,
+                                stdout = subprocess.PIPE,
+                                stderr = subprocess.PIPE
+                                )
+        (out, err)=task.communicate()
+        out_string=out.decode("utf-8") 
+        err_msg=err.decode("utf-8")
+        if err_msg:
+            print(err_msg)
+        md5file+=out_string
+    print(md5file)
+    print("poo")
+check('./S-BG-S2-Mathew/')
+
+# proc = subprocess.Popen(['echo', name],
+#                             stdin = subprocess.PIPE,
+#                             stdout = subprocess.PIPE,
+#                             stderr = subprocess.PIPE
+#                         )
+
+# (out, err) = proc.communicate()
+
+
+'''
+def (filepath)
+os.path
+
+'''
+
